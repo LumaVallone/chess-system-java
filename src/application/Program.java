015f4dc5566adf6bc8bfd1dda08b2515a43dab63
LumaVallone <lumavallonebras@hotmail.com>
2022-02-18 14:27:59 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word");
-
+		Position position = new Position(3,5);
+		System.out.println(position);
 	}
-
 }
